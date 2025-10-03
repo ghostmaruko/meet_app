f4360f2edb77b0b7122ff37c87ec87824e2e8250
Marco Esu <102921659+ghostmaruko@users.noreply.github.com>
2025-10-03 15:24:59 +0200

    update handler and serverless

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -2,9 +2,11 @@
 
 const { google } = require("googleapis");
 const calendar = google.calendar("v3");
+
 const SCOPES = [
   "https://www.googleapis.com/auth/calendar.events.public.readonly",
 ];
+
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = ["https://meet-app-silk.vercel.app/"];
 
@@ -14,7 +16,8 @@ const oAuth2Client = new google.auth.OAuth2(
   redirect_uris[0]
 );
 
-module.exports.getAuthURL = async (event = {}, context = {}) => {
+// === Funzione 1: Genera URL di autorizzazione ===
+module.exports.getAuthURL = async () => {
   try {
     const authUrl = oAuth2Client.generateAuthUrl({
       access_type: "offline",
@@ -35,4 +38,34 @@ module.exports.getAuthURL = async (event = {}, context = {}) => {
       body: JSON.stringify({ error: error.message }),
     };
   }
+};
+
+// === Funzione 2: Ottieni Access Token usando il code ===
+module.exports.getAccessToken = async (event) => {
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, tokens) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(tokens);
+    });
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: error.message }),
+      };
+    });
 };
