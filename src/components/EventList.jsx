c0b18e926e827290da3661f53f3e42bfa23e13fa
Marco Esu <102921659+ghostmaruko@users.noreply.github.com>
2025-10-06 14:27:43 +0200

    implementing test

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import Event from "./Event";
 
-
-const EventList = () => {
+const EventList = ({ events }) => {
   return (
-    <ul id="event-list"></ul>
+    <ul id="event-list">
+      {events
+        ? events.map((event) => <Event key={event.id} event={event} />)
+        : null}
+    </ul>
   );
-}
-
+};
 
 export default EventList;
