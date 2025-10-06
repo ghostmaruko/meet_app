26309cb5ead081a7a7d8fb1f226d3f4f07b80045
Marco Esu <102921659+ghostmaruko@users.noreply.github.com>
2025-10-06 14:55:29 +0200

    add citysearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
+
 const App = () => {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
---
+++ src/components/CitySearch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CitySearch = () => {
+  return (
+    <div id="city-search">
+      <input type="text" className="city" placeholder="Search for a city" />
+    </div>
+  );
+};
+
+export default CitySearch;
