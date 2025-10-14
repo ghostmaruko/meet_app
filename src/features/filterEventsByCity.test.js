e5a3f7b58788d5c9b7be512d3f9a72190fea0694
Marco Esu <102921659+ghostmaruko@users.noreply.github.com>
2025-10-14 17:00:03 +0200

    scenario one ready | test passed

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -20,7 +20,7 @@ defineFeature(feature, (test) => {
     });
 
     /* then("the user should see the list of all upcoming events.", () => {}); */
-    then("the user should see the list of upcoming events", async () => {
+    then("the user should see the list of all upcoming events.", async () => {
       const AppDOM = AppComponent.container.firstChild;
       const EventListDOM = AppDOM.querySelector("#event-list");
 
@@ -29,46 +29,46 @@ defineFeature(feature, (test) => {
         expect(EventListItems.length).toBe(32);
       });
     });
-  });
 
-  test("User should see a list of suggestions when they search for a city.", ({
-    given,
-    when,
-    then,
-  }) => {
-    given("the main page is open", () => {});
+    test("User should see a list of suggestions when they search for a city.", ({
+      given,
+      when,
+      then,
+    }) => {
+      given("the main page is open", () => {});
 
-    when("user starts typing in the city textbox", () => {});
+      when("user starts typing in the city textbox", () => {});
 
-    then(
-      "the user should receive a list of cities (suggestions) that match what they’ve typed",
-      () => {}
-    );
-  });
+      then(
+        "the user should receive a list of cities (suggestions) that match what they’ve typed",
+        () => {}
+      );
+    });
 
-  test("User can select a city from the suggested list.", ({
-    given,
-    and,
-    when,
-    then,
-  }) => {
-    given("user was typing “Berlin” in the city textbox", () => {});
+    test("User can select a city from the suggested list.", ({
+      given,
+      and,
+      when,
+      then,
+    }) => {
+      given("user was typing “Berlin” in the city textbox", () => {});
 
-    and("the list of suggested cities is showing", () => {});
+      and("the list of suggested cities is showing", () => {});
 
-    when(
-      "the user selects a city (e.g., “Berlin, Germany”) from the list",
-      () => {}
-    );
+      when(
+        "the user selects a city (e.g., “Berlin, Germany”) from the list",
+        () => {}
+      );
 
-    then(
-      "their city should be changed to that city (i.e., “Berlin, Germany”)",
-      () => {}
-    );
+      then(
+        "their city should be changed to that city (i.e., “Berlin, Germany”)",
+        () => {}
+      );
 
-    and(
-      "the user should receive a list of upcoming events in that city",
-      () => {}
-    );
+      and(
+        "the user should receive a list of upcoming events in that city",
+        () => {}
+      );
+    });
   });
 });
