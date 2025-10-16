54f79838ae36f922297ab8c1ae0a6113f471c4fb
Marco Esu <102921659+ghostmaruko@users.noreply.github.com>
2025-10-16 15:54:01 +0200

    remove error notify

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import * as atatus from "atatus-spa";
 atatus.config("16fc4beee9b040f7a92d9b77cbd63e7b").install();
-atatus.notify(new Error('Test Atatus Setup'));
-
+/* atatus.notify(new Error('Test Atatus Setup'));
+ */
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
